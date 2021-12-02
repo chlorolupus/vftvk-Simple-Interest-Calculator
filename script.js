04a6308b4dc25f056382077350a5157b57fc2b2a
Timmy <lokkiner@msn.com>
2021-12-02 18:08:31 +0800

    First Commit

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let p = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value / 100.0;
+    let rateOnScreen = document.getElementById("rate").value;
+    let years = parseInt(document.getElementById("years").value);
+    let interest = 0.00;
+    interest = p * rate * years;
+    let year = new Date().getFullYear();
+    year = year+years;
+    total = interest+p;
+    result = document.getElementById("result");
+    result.textContent = "If you deposit " + p + ", at an interest rate of " 
+            + rateOnScreen + "%. You will receive an amount of " 
+            + interest +",in the year " + year;
 }
         
+function changeInterest()
+{   
+    let rate = document.getElementById("rate").value;
+    let interestRate = document.getElementById("interestRate");
+    interestRate.textContent = rate +"%";
+}
