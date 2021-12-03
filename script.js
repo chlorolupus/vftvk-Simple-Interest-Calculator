5502c33cf9a2816d7567b3d25b28bcc4b7770289
Timmy <lokkiner@msn.com>
2021-12-03 14:42:09 +0800

    Reload script.js

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 function compute()
 {
     let p = document.getElementById("principal").value;
-    
-    let rate = document.getElementById("rate").value / 100.0;
-    let rateOnScreen = document.getElementById("rate").value;
-    let years = parseInt(document.getElementById("years").value);
-    let interest = 0.00;
-    interest = p * rate * years;
-    let year = new Date().getFullYear();
-    year = year+years;
-    total = interest+p;
-    
-    deposit = document.getElementById("deposit");
-    deposit.textContent = "If you deposit " + p + ",";
-    interestratetext = document.getElementById("interestratetext");
-    interestratetext.textContent = " at an interest rate of " + rateOnScreen + "%.";
-    finalamount = document.getElementById("finalamount");
-    finalamount.textContent = "You will receive an amount of " +interest + ",";
-    finalyear =  document.getElementById("finalyear");
-    finalyear.textContent = "in the year " + year;
-    
+    if(p>0){
+        let rate = document.getElementById("rate").value / 100.0;
+        let rateOnScreen = document.getElementById("rate").value;
+        let years = parseInt(document.getElementById("years").value);
+        let interest = 0.00;
+        interest = p * rate * years;
+        let year = new Date().getFullYear();
+        year = year+years;
+        total = interest+p;
+
+        deposit = document.getElementById("deposit");
+        deposit.textContent = "If you deposit " + p + ",";
+        interestratetext = document.getElementById("interestratetext");
+        interestratetext.textContent = " at an interest rate of " + rateOnScreen + "%.";
+        finalamount = document.getElementById("finalamount");
+        finalamount.textContent = "You will receive an amount of " +interest + ",";
+        finalyear =  document.getElementById("finalyear");
+        finalyear.textContent = "in the year " + year;
+}
+    else
+    {
+        alert("Please enter positive number in amount");
+    }
 }
         
 function changeInterest()
